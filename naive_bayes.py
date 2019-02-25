bf9e7708aaee00cb4553106f5645924f3dbdfad0
Tom Rein <tomjrein@gmail.com>
2019-02-24 20:09:33 -0500

    initial classification

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,8 @@
 import numpy as np
+import math
 from math import ceil
+
+np.set_printoptions(suppress=True)
 
 def isolate_sets(training, testing):
     train_y, train_x = separate_data(training)
@@ -33,6 +36,12 @@ def handle_data(data):
 
     return isolate_sets(training, testing)
 
+def normpdf(x, mean, sd):
+    var = float(sd)**2
+    denom = (2*math.pi*var)**.5
+    num = math.exp(-(float(x)-float(mean))**2/(2*var))
+    return num/denom
+
 def main():
     data = np.genfromtxt('./spambase.data', delimiter=',', dtype="uint16")
     train_x, train_y, test_x, test_y  = handle_data(data)
@@ -41,10 +50,47 @@ def main():
     not_spam = []
     for i, obs in enumerate(train_x):
         if train_y[i] == 1:
-            not_spam.append(obs)
-        else:
             spam.append(obs)
+        else:
+            not_spam.append(obs)
+    
+    spam_prior = len(spam) / len(train_x)
+    not_spam_prior = len(not_spam) / len(train_x)
 
+    spam = np.array(spam)
+    not_spam = np.array(not_spam)
+
+    spam_mean = np.mean(spam, axis=0)
+    spam_std = np.std(spam, axis=0)
+
+    not_spam_mean = np.mean(not_spam, axis=0)
+    not_spam_std = np.std(not_spam, axis=0)
+
+    labels = []
+    for i, obs in enumerate(test_x):
+
+        pos_prob = spam_prior
+        neg_prob = not_spam_prior
+
+        for j, feature in enumerate(obs):
+
+            pos_mean = spam_mean[j]
+            pos_std = spam_std[j]
+            neg_mean = not_spam_mean[j]
+            neg_std = not_spam_std[j]
+
+            pos_prob *= normpdf(feature, pos_mean, pos_std)
+            neg_prob *= normpdf(feature, neg_mean, neg_std)
+
+        if pos_prob > neg_prob:
+            labels.append(1)
+        else:
+            labels.append(0)
+
+    print("all done")
+    labels = np.array(labels)
+    for x, label in enumerate(labels):
+        print(label, test_y[x])
 
 if __name__ == '__main__':
     main()
