94a9ddc5ca7fd46786057c52b79c1bdcdbc28b5f
root <root@LAPTOP-HH374M69.localdomain>
2019-03-02 13:05:02 -0500

    add display function

--- spam_id3.py
+++ spam_id3.py
@@ -2,6 +2,32 @@ import numpy as np
 from data_operations import handle_data
 from id3_functions import train, dtl, traverse_tree
 
+def display_performance(labels, test_y):
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+
+    for x, prediction in enumerate(labels):
+        target = test_y[x]
+
+        if prediction == 1 and target == 1:
+            tp += 1
+        if prediction == 1 and target == 0:
+            fp += 1
+        if prediction == 0 and target == 0:
+            tn += 1
+        if prediction == 0 and target == 1:
+            fn += 1
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+    print ("Precision:", precision)
+    print ("Recall:", recall)
+    print ("F-measure:", (2 * precision * recall) / (precision + recall))
+    print ("Accuracy:", (tp + tn) / (tp + tn + fp + fn))
+
 def main():
     data = np.genfromtxt('./spambase.data', delimiter=',')
     train_x, train_y, test_x, test_y  = handle_data(data)
@@ -14,35 +40,7 @@ def main():
     for i, obs in enumerate(test_x):
         labels.append(traverse_tree(tree, obs))
 
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
-
-    labels = np.array(labels)
-    for x, prediction in enumerate(labels):
-        target = test_y[x]
-
-        if prediction == 1 and target == 1:
-            tp += 1
-
-        if prediction == 1 and target == 0:
-            fp += 1
-
-        if prediction == 0 and target == 0:
-            tn += 1
-
-        if prediction == 0 and target == 1:
-            fn += 1
-
-    print("tp", tp)
-    print("tn", tn)
-    print("fp", fp)
-    print("fn", fn)
-
-    print ("precision", tp / (tp + fp))
-    print ("recall", tp / (tp + fn))
-    print ((tp + tn) / (tp + tn + fp + fn))
+    display_performance(labels, test_y)
 
 if __name__ == '__main__':
     main()
